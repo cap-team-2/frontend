867e374796ef75cd2a7e7b10a9724d7546bc9aa5
Kerridene Small <Kerridenesmall@gmail.com>
2023-09-05 12:04:34 -0400

    Add api template

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,5 +1,20 @@
 // Products.jsx
+import { useState, useEffect } from "react";
+import axios from "axios";
+const API = process.env.REACT_APP_API_URL
 
-export default function Products() {
+export default function Products( ) {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    axios.get(`${API}/products`)
+      .then((res) => {
+        console.log(res)
+        // setProducts()
+      })
+  }, []);
+
+
+
   return <div><h1>Products</h1></div>;
 }
