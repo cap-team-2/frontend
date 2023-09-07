2afaaa10eec713c5de436003163b6e66365efd21
joshuanelsondev <joshuanelsondev@gmail.com>
2023-09-07 12:32:14 -0400

    Edit loginform validations

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -2,6 +2,13 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
+import {
+  AiOutlineEye,
+  AiOutlineEyeInvisible,
+  AiOutlineCheck,
+} from "react-icons/ai";
+import { HiOutlineCheck } from "react-icons/hi";
+
 const API = import.meta.env.VITE_APP_API_URL;
 
 export default function LoginForm() {
@@ -10,7 +17,12 @@ export default function LoginForm() {
         password: "", 
     });
 
-    const [errorMsg, setErrorMsg] = useState("");
+    const [passwordType, setPasswordType] = useState('password');
+
+    // Function that toggles the passwordType from 'password' to 'text' creating a show password effect
+    const togglePassword = (type) => {
+      type === 'password' ? setPasswordType('text') : setPasswordType('password');
+    }
 
     // Function to update the form state with every change for login form
     const handleFormChange = (event) => {
@@ -36,12 +48,12 @@ export default function LoginForm() {
               Create an Account
             </Link>
           </div>
-          <form className="grid gap-4 mb-8">
+          <form className="grid gap-4 mb-8 relative">
             <div className="grid gap-2">
               <label htmlFor="email">Email Address</label>
               <input
                 placeholder="yourname@domain.com"
-                className="peer h-12 border placeholder:text-[#5a5a5a] pl-4 outline-none ease-in-out duration-500 focus:ring invalid:border-[red] focus:border-none"
+                className="peer h-12 border placeholder:text-[#5a5a5a] pl-4 outline-none ease-in-out duration-500 focus:ring invalid:border-b-2 invalid:border-b-[red] focus:border-blue-light "
                 onChange={handleFormChange}
                 value={login.email}
                 type="email"
@@ -50,21 +62,37 @@ export default function LoginForm() {
               <p className="text-xs text-[red] invisible peer-invalid:visible peer-focus:invisible">
                 Please enter a valid email
               </p>
+              <HiOutlineCheck className="absolute peer-placeholder-shown:!invisible peer-invalid:invisible peer-valid:visible top-[45px] right-2 text-green-dark text-xl" />
             </div>
-            <div className="grid gap-2 mb-4">
+            <div className="grid gap-2 mb-4 relative">
               <label htmlFor="password">Password</label>
               <input
                 placeholder="Password"
-                className="peer h-12 border placeholder:text-[#5a5a5a] pl-4 outline-none focus:ring ease-in-out duration-500 invalid:border-[red] focus:border-none"
+                className="peer h-12 border placeholder:text-[#5a5a5a] pl-4 outline-none focus:ring ease-in-out duration-500 invalid:border-b-2 invalid:border-b-[red] focus:border-blue-light"
                 onChange={handleFormChange}
                 value={login.password}
-                type="password"
+                type={passwordType}
                 id="password"
-                required
+                maxLength={40}
+                pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$"
               />
               <p className="text-xs text-[red] invisible peer-invalid:visible peer-focus:invisible">
-                Please enter a password
+                Password must contain at least 8 characters, including one
+                uppercase letter, one lowercase letter, and one digit.
               </p>
+              <AiOutlineEye
+                className={`absolute top-11 right-10 text-2xl cursor-pointer ${
+                  passwordType === "password" ? "visible" : "invisible"
+                }`}
+                onClick={() => togglePassword("password")}
+              />
+              <AiOutlineEyeInvisible
+                className={`absolute top-11 right-10 text-2xl cursor-pointer ${
+                  passwordType === "text" ? "visible" : "invisible"
+                }`}
+                onClick={() => togglePassword("text")}
+              />
+              <HiOutlineCheck className="absolute peer-placeholder-shown:!invisible peer-invalid:invisible peer-valid:visible top-[45px] right-2 text-green-dark text-xl" />
             </div>
             <div className="grid text-center gap-2 text-sm">
               <button
