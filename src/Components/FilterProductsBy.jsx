0d979b77a1142e30dc8d7e6c8aa831c2bf637c54
joshuanelsondev <joshuanelsondev@gmail.com>
2023-09-15 16:12:51 -0400

    Add FilterProductsBy.jsx; use this to add products images and reacticons for the filter options on the home page

---
+++ src/Components/FilterProductsBy.jsx
@@ -0,0 +1,18 @@
+// FilterProductsBy.jsx
+
+// Array of various products to be used as filter buttons on the home page
+const products = [
+    { category: '', button: '', image: '' }
+]
+
+
+
+export default function FilterProductsBy() {
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
