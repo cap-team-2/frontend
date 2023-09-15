119e9792b2f7f9d37428f93f48be0423dc70f30e
joshuanelsondev <joshuanelsondev@gmail.com>
2023-09-15 16:13:15 -0400

    Update FilterProductsByu

--- src/Components/FilterProductsBy.jsx
+++ src/Components/FilterProductsBy.jsx
@@ -2,7 +2,7 @@
 
 // Array of various products to be used as filter buttons on the home page
 const products = [
-    { category: '', button: '', image: '' }
+    { category: '', icon: '', image: '' }
 ]
 
 
