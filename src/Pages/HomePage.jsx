7c6d9f291b62cec157ef7766058f70c356edc286
joshuanelsondev <joshuanelsondev@gmail.com>
2023-09-19 11:52:20 -0400

    Edit styling for products card

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,18 +7,24 @@ export default function HomePage({searchResults}) {
 
 
     return (
-        <div className="h-full w-full">
-            <FilterProductsBy />
-            <h1>Home</h1>
-            {searchResults ? searchResults.map((results) => {
-                return (
-                    <div key={results.id}>
-                        <h2>{results.name}</h2>
-                        {/* <img src={results.image} alt={results.description} /> */}
-                    </div>
-                )
-            }): <h2>Results Not Found</h2> }
-
+      <div className="h-full w-full">
+        <FilterProductsBy />
+        <div className="grid justify-center gap-10 mt-6">
+          {searchResults ? (
+            searchResults.map((results) => {
+              return (
+                <div key={results.id} className="grid justify-center gap-4 h-auto w-48">
+                  <img src={results.image} alt={results.description} className="h-48 w-48 justify-self-center rounded-lg shadow-sm shadow-green-dark" />
+                  <p className="text-md font-bold">{results.description} - {results.weight}{results.unit_measurement}</p>
+                  <p className="text-xs font-medium">${results.cost}</p>
+                  <button className="bg-green-light rounded text-xs text-white font-semibold h-8 w-full justify-self-center">Add to cart</button>
+                </div>
+              );
+            })
+          ) : (
+            <h2>Results Not Found</h2>
+          )}
         </div>
-    )
+      </div>
+    );
 }
