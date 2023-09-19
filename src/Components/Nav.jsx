5196b4178e527556ecfde3e2360c2eea6152af87
joshuanelsondev <joshuanelsondev@gmail.com>
2023-09-19 13:01:37 -0400

    Move nav functions to the search component

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,54 +2,15 @@
 // Nav.jsx
 
 import { Link } from 'react-router-dom';
-import { useState, useEffect, useCallback } from 'react';
 import PantriLogo from '../assets/Pantri-logo-removebg.png';
 import {
   MdMenu,
 } from "react-icons/md";
 import { BsBag } from "react-icons/bs";
-import axios from "axios";
 import SearchBar from './SearchBar';
-const API = import.meta.env.VITE_APP_API_URL;
 
-export default function Nav({setSearchResults}) {
-  const [searchKey, setSearchKey] = useState("");
-
-  function handleSearchChange(event) {
-    setSearchKey(event.target.value);
-  }
-
-  const search = useCallback(async () => {
-    await performSearch(searchKey);
-
-  }, [searchKey]); 
-
-  useEffect(() => {
-    search();
-  }, [search]); 
-
-  async function performSearch(searchKey) {
-    axios.get(`${API}/search/${searchKey}`)
-      .then((res) => {
-        if(res.data.length) {
-          setSearchResults(res.data);
-        } else {
-          setSearchResults(false);
-          console.log('false')
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-  }
-
-  function handleSearchSubmit(event) {
-    event.preventDefault(); 
-    search(); 
-  }
-
-
-
+export default function Nav() {
+  
   return (
     <div className="h-auto w-full flex flex-col border-b-2 border-b-gray-light">
       <div className="grid grid-cols-3 md:flex md:justify-between md:px-8 items-center top-5 px-2 md:mt-10 md:mb-2">
@@ -60,13 +21,19 @@ export default function Nav({setSearchResults}) {
         {/* Logo that links back to homepage */}
         <div className="flex items-center justify-center">
           <Link to={"/"}>
-            <img src={PantriLogo} alt="Pantri Logo" className="h-20 md:hidden" />
-            <p className='text-black text-3xl hidden md:block md:text-green-light'>PANTRI</p>
+            <img
+              src={PantriLogo}
+              alt="Pantri Logo"
+              className="h-20 md:hidden"
+            />
+            <p className="text-black text-3xl hidden md:block md:text-green-light">
+              PANTRI
+            </p>
           </Link>
         </div>
         {/* Search Bar */}
-        <div className='h-auto w-full hidden md:block'>
-          <SearchBar handleSearchChange={handleSearchChange} handleSearchSubmit={handleSearchSubmit} />
+        <div className="h-auto w-full hidden md:block">
+          <SearchBar setSearchResults={setSearchResults} />
         </div>
         {/* Nav Links */}
         <div className="flex items-center justify-end gap-4">
@@ -87,8 +54,8 @@ export default function Nav({setSearchResults}) {
           </Link>
         </div>
       </div>
-      <div className='md:hidden'>
-        <SearchBar handleSearchChange={handleSearchChange} handleSearchSubmit={handleSearchSubmit} />
+      <div className="md:hidden">
+        <SearchBar setSearchResults={setSearchResults} />
       </div>
     </div>
   );
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,9 +1,47 @@
 // SearchBar.jsx
+import { useState, useEffect, useCallback } from "react";
+import axios from "axios";
 import { IoSearchCircleSharp } from "react-icons/io5";
+const API = import.meta.env.VITE_APP_API_URL;
 
 
-export default function SearchBar({handleSearchSubmit, handleSearchChange}) {
-    
+export default function SearchBar({setSearchResults}) {
+    const [searchKey, setSearchKey] = useState("");
+
+    function handleSearchChange(event) {
+      setSearchKey(event.target.value);
+    }
+
+    const search = useCallback(async () => {
+      await performSearch(searchKey);
+    }, [searchKey]);
+
+    useEffect(() => {
+      search();
+    }, [search]);
+
+    async function performSearch(searchKey) {
+      axios
+        .get(`${API}/search/${searchKey}`)
+        .then((res) => {
+          if (res.data.length) {
+            setSearchResults(res.data);
+          } else {
+            setSearchResults(false);
+            console.log("false");
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    function handleSearchSubmit(event) {
+      event.preventDefault();
+      search();
+    }
+
+
     return (
       <>
         {/*search bar*/}
