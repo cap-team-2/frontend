423b994eb766e1fe5286e0a6daeb1312c5d522cb
joshuanelsondev <joshuanelsondev@gmail.com>
2023-09-18 22:59:01 -0400

    Edit homepage search

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,20 +1,24 @@
 // Home.jsx
 
 import FilterProductsBy from "../Components/FilterProductsBy";
+import { useState, useEffect } from 'react';
 
 export default function HomePage({searchResults}) {
+    const 
+
     return (
         <div className="h-full w-full">
             <FilterProductsBy />
             <h1>Home</h1>
-            {searchResults.map((results) => {
+            {console.log(searchResults.length)}
+            {searchResults.length ? searchResults.map((results) => {
                 return (
-                    <div>
-                        <h1>{results.name}</h1>
+                    <div key={results.id}>
+                        <h2>{results.name}</h2>
                         {/* <img src={results.image} alt={results.description} /> */}
                     </div>
                 )
-            })}
+            }): <h2>Results Not Found</h2> }
 
         </div>
     )
