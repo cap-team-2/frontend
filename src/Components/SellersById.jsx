626fe10780ea4f72774b94dd7d6e2b741e30c071
Kerridene Small <Kerridenesmall@gmail.com>
2023-09-21 10:36:21 -0400

    Created sellersbyid componenet Updated routes of sellers in app.jsx Created api call for sellersbyid

--- src/Components/SellersById.jsx
+++ src/Components/SellersById.jsx
@@ -1,30 +1,27 @@
-// ProductById.jsx
 import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import axios from "axios";
 
 const API = import.meta.env.VITE_APP_API_URL;
 
-export default function ProductById() {
-    const [product, setProduct] = useState([]);
+export default function SellersById() {
+    const [seller, setSeller] = useState([])
     const { id } = useParams();
 
     useEffect(() => {
         axios
-            .get(`${API}/products/${id}`)
+            .get(`${API}/sellers/${id}`)
             .then((res) => {
                 console.log(res.data);
-                setProduct(res.data);
+                setSeller(res.data);
             })
             .catch((error) => {
                 console.log(error);
             })
     }, [])
 
-
     return (
         <div>
-
         </div>
     )
 }
