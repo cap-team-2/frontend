ff6329d049b41069302711c9bcc724a54ad41547
joshuanelsondev <joshuanelsondev@gmail.com>
2023-09-22 17:48:09 -0400

    Add banner image as a background in tailwind config file

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,20 +1,17 @@
 // Home.jsx
 
 import FilterProductsBy from "../Components/FilterProductsBy";
-import { useState, useEffect } from 'react';
 import SearchResults from "../Components/SearchResults";
-import BannerImage from "../assets/background-ideas/engin-akyurt-Y5n8mCpvlZU-unsplash.jpg";
+
 
 export default function HomePage({searchResults}) {
 
 
     return (
       <div className="h-full w-full flex flex-col ">
-        <div className="self-center tablet:w-3/5">
-          <img src={BannerImage} alt="Banner Image of assorted vegetables" />
+        <div className="self-center">
           <FilterProductsBy />
         </div>
-
         <div>
           <SearchResults searchResults={searchResults} />
         </div>
