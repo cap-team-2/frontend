4126c5fbf00c1e52eaa719214034c7133678cb34
joshuanelsondev <joshuanelsondev@gmail.com>
2023-09-26 14:44:38 -0400

    Edit nav bar

--- src/Components/FilterProductsBy.jsx
+++ src/Components/FilterProductsBy.jsx
@@ -36,7 +36,7 @@ export default function FilterProductsBy({setSearchResults, filter, setFilter })
                   key={generateId()}
                   onClick={() => filterProducts(productFilter)}
                   >
-                    <productFilter.icon className="text-2xl md:text-3xl text-green-light" />
+                    <productFilter.icon className="text-2xl md:text-3xl text-green-light"/>
                     <p className="text-xs tablet:text-sm">{productFilter.category}</p>
                   </div>
                 );
