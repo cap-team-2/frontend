76dbbb8d14142367fdce7443e02f72d9d1fb996e
joshuanelsondev <joshuanelsondev@gmail.com>
2023-09-28 00:45:41 -0400

    Edit products page

--- src/Components/ProductDetails.jsx
+++ src/Components/ProductDetails.jsx
@@ -10,13 +10,14 @@ export default function ProductById() {
     const [product, setProduct] = useState({});
     const [quantity, setQuantity] = useState(1);
     const { id } = useParams();
+    const costPerUnitWeight = (product.cost / product.weight).toFixed(2);
 
     // Make an api call to retrieve a product with the given id
     useEffect(() => {
         axios
             .get(`${API}/products/${id}`)
             .then((res) => {
-                // setProduct(res.data);
+                setProduct(res.data);
             })
             .catch((error) => {
                 console.log(error);
@@ -26,18 +27,19 @@ export default function ProductById() {
 
     return (
         
-      <div className="h-full w-full mt-24 flex justify-center">
-        {Object.keys(product).length !== 0 ? ( <div className="h-full w-full flex flex-col p-4">
+      <div className="h-full w-full">
+        {Object.keys(product).length !== 0 ? ( 
+        <div className="h-screen w-full flex flex-col justify-between px-4 pb-8 pt-24 gap-8 ">
           <div className="p-4">
             <img
               src={product.image}
               alt={product.description}
-              className="h-auto tablet:h-96 w-auto mb-8 shadow-2xl rounded-xl"
+              className="h-auto tablet:h-96 shadow-2xl rounded-xl"
             />
           </div>
           <div className="">
             <h2 className="text-2xl font-bold">{capitalize(product.name)}</h2>
-            <div>
+            <div className="flex flex-col">
               <p
                 className={`${
                   product.stock > 10
@@ -53,11 +55,11 @@ export default function ProductById() {
                   ? "● Only " + product.stock + " left"
                   : "● Out of Stock"
               }`}</p>
-              <div className="mt-4">
-                <h3 className="font-medium">Description</h3>
-                <p className="text-[gray]">{product.description}</p>
+              <div className="mt-4 flex flex-col gap-2">
+                <h3 className="font-medium text-base">Description</h3>
+                <p className="text-[gray] text-sm">{product.description}</p>
               </div>
-              <div className="flex">
+              <div className="flex justify-between">
                 <p className="text-2xl font-semibold relative -z-0">
                   <span className="text-3xl">
                     ${`${product.cost.split(".")[0]}`}
@@ -66,7 +68,7 @@ export default function ProductById() {
                     {product.cost.split(".")[1]}
                   </span>
                   <span className="pl-4 text-[gray] text-sm font-normal">
-                    {/* ({costPerUnitWeight}/{product.unit_measurement}) */}
+                    ({costPerUnitWeight}/{product.unit_measurement})
                   </span>
                 </p>
                 <button className="bg-gray-light">Qty: {quantity}</button>
