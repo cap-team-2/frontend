49b99047443203dd325b8ff1dc08d41da9947a2f
joshuanelsondev <joshuanelsondev@gmail.com>
2023-10-10 14:57:52 -0400

    Add redirects

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // Base public path when served in production (e.g., "/my-app/").
-  base: "/",
-  
-  publicDir: "frontend/public",
-
-  // Define the entry point of your application.
+  base: '/',
+  publicDir: 'frontend/public',
   entry: {
-    main: "src/main.jsx",
+    main: 'src/main.jsx',
   },
-
   build: {
-    outDir: "dist",
-    assetsDir: "assets",
+    outDir: 'dist',
+    assetsDir: 'assets',
     sourcemap: true,
   },
-
-  // Configure the development server options.
   server: {
     port: 3333,
     open: true,
   },
-
-  // Configure how Vite handles imports and resolves files.
   resolve: {
     alias: {
-      // Create aliases for commonly used paths.
-      "@": "/src",
+      '@': '/src',
     },
   },
-
-  // Configure plugins for Vite.
   plugins: [react()],
 });
