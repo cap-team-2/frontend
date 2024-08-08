417d439e0b3bde010d206e1baddee988765275d2
joshuanelsondev <joshuanelsondev@gmail.com>
2024-08-07 22:59:31 -0400

    refactor: edit styling of the filter buttons and containers

--- src/Components/filter/FilterProductsBy.jsx
+++ src/Components/filter/FilterProductsBy.jsx
@@ -1,29 +1,29 @@
 /* eslint-disable react/prop-types */
 // FilterProductsBy.jsx
 
-import { v4 as generateId } from "uuid";
-import ProductFilters from "./ProductFilters";
+import { v4 as generateId } from 'uuid'
+import ProductFilters from './ProductFilters'
 
 export default function FilterProductsBy({ filterProducts, filter }) {
-  
-    return (
-      <div className="flex justify-between w-full gap-10 desktop:px-10 xl:px-48">
-        {ProductFilters.map((productFilter) => {
-          return (
-            <button
-              className={`h-16 w-20 flex flex-col items-center justify-start gap-2 hover:underline hover:underline-offset-8 decoration-2 cursor-pointer shrink-0 ${
-                filter.current === productFilter.category
-                  ? "underline  underline-offset-8 decoration-green"
-                  : "hover:decoration-gray"
-              }`}
-              key={generateId()}
-              onClick={() => filterProducts(productFilter.category)}
-            >
-              <productFilter.icon className="text-2xl md:text-3xl text-green" />
-              <p className="text-xs tablet:text-sm">{productFilter.category}</p>
-            </button>
-          );
-        })}
-      </div>
-    );
+  return (
+    <div className="flex justify-between w-full gap-10 desktop:px-10 xl:px-48">
+      {ProductFilters.map((productFilter) => {
+        return (
+          <button
+            id="filter-button"
+            className={`h-16 w-20 flex flex-col items-center justify-start gap-2 hover:underline hover:underline-offset-8 decoration-2 cursor-pointer shrink-0 ${
+              filter.current === productFilter.category
+                ? 'underline  underline-offset-8 decoration-green'
+                : 'hover:decoration-gray'
+            }`}
+            key={generateId()}
+            onClick={() => filterProducts(productFilter.category)}
+          >
+            <productFilter.icon className="text-2xl text-green" />
+            <p className="text-sm">{productFilter.category}</p>
+          </button>
+        )
+      })}
+    </div>
+  )
 }
