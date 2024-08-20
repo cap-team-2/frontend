5d610be6241fe2eba0568df4479010c5aacff6ae
joshuanelsondev <joshuanelsondev@gmail.com>
2024-08-19 21:43:33 -0400

    feat: create resultsStyles.js file to store tailwind styles

---
+++ src/Components/search/resultsStyles.js
@@ -0,0 +1,5 @@
+export const mobile = 'mobile:grid-cols-1'
+export const mobileMd = 'mobileMd:grid-cols-2'
+export const tablet = 'tablet:grid-cols-3'
+export const laptop = 'laptop:grid-cols-4'
+export const productsContainer = `grid justify-center w-full gap-8 py-4 ${mobile} ${mobileMd} ${tablet} ${laptop}`
